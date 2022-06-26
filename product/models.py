8fbc38ec875c34dfc8acffc7a4618c881efe1fc1
markofrontend <markovucurovic@gmail.com>
2022-06-26 16:31:45 +0200

    Update absolute urls for category & product

--- product/models.py
+++ product/models.py
@@ -17,7 +17,7 @@ class Category(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return f'/{self.slug}/'
+        return f'/products/{self.slug}/'
 
 
 class Product(models.Model):
@@ -37,7 +37,7 @@ class Product(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return f'/{self.category.slug}/{self.slug}/'
+        return f'/products/{self.category.slug}/{self.slug}/'
 
     def get_image(self):
         if self.image:
