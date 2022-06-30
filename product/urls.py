09b07478f4c5d7aaafec78a91eb7af5471c8bf7a
markofrontend <markovucurovic@gmail.com>
2022-06-30 18:00:25 +0200

    Search products view

--- product/urls.py
+++ product/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('categories/', views.CategoryList.as_view()),
     path('products/', views.ProductList.as_view()),
+    path('products/search/', views.search),
     path('products/<slug:category_slug>/', views.CategoryDetail.as_view()),
     path('products/<slug:category_slug>/<slug:product_slug>/', views.ProductDetail.as_view()),
 ]
