a75c0c2655f3bfa5213695160221140825ae1ddf
markofrontend <markovucurovic@gmail.com>
2022-07-03 20:49:03 +0200

    JWT tokens lifetime, backend cors origin

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -52,6 +53,7 @@ INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8081",
+    "http://127.0.0.1:8000",
 ]
 
 MIDDLEWARE = [
@@ -93,6 +95,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'AUTH_HEADER_TYPES': ('JWT',),
 }
 
